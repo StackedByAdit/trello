fc35a0f1aee7ba2d7788363ee0fa508f65334aee
Aditya <adityaguptavns1@gmail.com>
2026-08-24 22:43:24 +0530

    feat(ws): new ws imports share db

--- apps/websocket/index.ts
+++ apps/websocket/index.ts
@@ -2,6 +2,51 @@ import { WebSocketServer } from "ws";
 
 const server = new WebSocketServer({ port: 8080 });
 
+const BOARD: any = {};
+
 server.on("connection", (socket) => {
+    let joinedRoom = null;
+    socket.on("message", (data) => {
+        const parsedData = JSON.parse(data);
+
+        if (parsedData.type === "join") {
+            const boardId = parsedData.boardId;
+            socket.roomId = boardId;
+
+            if (!BOARD[boardId]) {
+                BOARD[boardId] = [];
+            }
+
+            const newUserId = Math.random();
+
+            for (let i = 0; i < BOARD[boardId].length; i++) {
+                const user = BOARD[boardId][i];
+
+                user.socket.send(
+                    JSON.stringify({
+                        type: "join",
+                        userId: newUserId,
+                    })
+                );
+            }
+
+            BOARD[boardId].push({
+                userId: newUserId,
+                socket: socket,
+            });
+
+            socket.send(
+                JSON.stringify({
+                    type: "initial_state",
+                    users: BOARD[boardId]
+                        .filter((x) => x.id != newUserId)
+                        .map((u) => u.id),
+                })
+            );
+        }
+    });
     
-})
+    socket.on("close", () => {
+           
+    } )
+});
